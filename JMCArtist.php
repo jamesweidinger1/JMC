7727ee7c8eb42cd5c3b59997e7af4a16f45ab1e7
stirce <31197311+stirce@users.noreply.github.com>
2017-08-21 13:05:31 +1000

    Create JMCArtist.php

---
+++ JMCArtist.php
@@ -0,0 +1,17 @@
+<html>
+<div style="position: absolute; top: 0px; left: 0px; width: 100%; outline: 1px solid black; background-color: azure;">
+    </br>
+    <a href="JMCHome.php"> Home</a>
+	<a href="JMCCommunity.php"> Community</a>
+	<a href="JMCCharts.php"> Charts</a>
+	<a href="JMCProfile.php"> Profile</a>
+</br>
+</br>
+</div>
+</br></br></br>
+<head>Jim's Music collection</head>
+<body>
+	<h1>Return to home</h1>
+	<p><a href="JMCHome.php">Home</a></p>
+</body>
+</html>
